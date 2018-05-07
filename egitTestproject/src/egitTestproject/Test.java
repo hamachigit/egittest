3e977da92528e77b59b055cb863aca3c3890eb77
hh <hh@DESKTOP-R267O9O.mshome.net>
2018-05-08 01:12:23 +0900

    コミットプッシュ

--- egitTestproject/src/egitTestproject/Test.java
+++ egitTestproject/src/egitTestproject/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("ブランチ");
+		System.out.println("新規");
 		System.out.println("ブランチ");
 	}
 
