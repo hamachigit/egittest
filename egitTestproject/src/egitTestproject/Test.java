0907d9bca00b29b2979c2d0c1c691ee573ceefa4
hh <hh@DESKTOP-R267O9O.mshome.net>
2018-05-08 22:00:07 +0900

    コミット

--- egitTestproject/src/egitTestproject/Test.java
+++ egitTestproject/src/egitTestproject/Test.java
@@ -8,12 +8,12 @@ public class Test {
 		System.out.println("ブランチ");
 	}
 
-	private void test3() {
+	private void test1() {
 	}
 
 	private void test2() {
 	}
 
-	private void test1() {
+	private void test3() {
 	}
 }
