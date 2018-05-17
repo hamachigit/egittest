6f0a3920dd7f5d42cf820dcad35be1b5172f6dbd
hamachi <hamachi@act-sys.com>
2018-05-17 18:49:45 +0900

    コミットメッセージ

--- egitTestproject/src/egitTestproject/Test.java
+++ egitTestproject/src/egitTestproject/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// テスト
-		System.out.println("テスト");
-		System.out.println("テスト");
+		System.out.println("変更する");
+		System.out.println("変更する");
 	}
 }
