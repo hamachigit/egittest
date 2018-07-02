ece165255b1960e5887d17ec9b8f88bbdc1a2dc7
hamachi <hamachi@act-sys.com>
2018-07-02 09:58:34 +0900

    fix #2 テスト

--- egitTestproject/src/egitTestproject/Test.java
+++ egitTestproject/src/egitTestproject/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// テスト
 		System.out.println("issue");
-		System.out.println("issue確認");
+		System.out.println("issue確認２");
 		System.out.println("衝突");
 	}
 }
